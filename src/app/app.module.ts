b69bfdf90775c52b0a713bfe4ba77a3054658758
fjmartinez <fjmartinezduran@gmail.com>
2021-02-16 20:02:35 +0100

    fin de pruebas intermedias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
+import { IncrementadorComponent } from './intermedio2/incrementador/incrementador.component';
+import { MedicoService } from './intermedio2/medico/medico.service';
 import { MedicosComponent } from './intermedio/espias/medicos.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MedicoComponent } from './intermedio2/medico/medico.component';
+import { HospitalComponent } from './intermedio2/hospital/hospital.component';
 
 @NgModule({
   declarations: [
-    AppComponent, MedicosComponent
+    AppComponent, MedicosComponent, MedicoComponent, HospitalComponent, IncrementadorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [MedicoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
